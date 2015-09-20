363f5190768f7cd39e08c5d27fc71ef57c010260
Vlad Polyakov <bericode@bk.ru>
2015-09-20 17:09:07 +0300

    Update view.php

--- view.php
+++ view.php
@@ -1,23 +1,22 @@
 <?php
-
-function view($file, $data = array(), $return = FALSE) {
-  $file = $file . '.php';
- 
-  extract($data);
- 
-  ob_start();
-
-  if (file_exists($file)) {
-    include $file;
-  } else {
-    print '<p class="error">Error: ' . $file . ' does not exists!</p>';
+function view($file, $data = array(), $return = false)
+{
+    $file = $file . '.php';
+    extract($data);
+    ob_start();
+    
+    if (file_exists($file)) {
+        include $file;
+    } else {
+        print '<p class="error">Error: ' . $file . ' does not exists!</p>';
     }
-        if ($return == TRUE) {
-            $buffer = ob_get_contents();
-            ob_end_clean();
-            return $buffer;
-        }
-
-        ob_end_flush();
+    
+    if ($return == true) {
+        $buffer = ob_get_contents();
+        ob_end_clean();
+        return $buffer;
     }
+    
+    ob_end_flush();
+}
 ?>
